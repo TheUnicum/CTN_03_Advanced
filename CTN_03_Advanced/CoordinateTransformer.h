6426ebba36ff9f099f45c293fcad3087e7836fd5
TheUnicum <tia_ben@tin.it>
2019-12-23 14:19:37 +0100

    [T2.2] - (9:47) <drawable sexiness>

--- CTN_03_Advanced/CoordinateTransformer.h
+++ CTN_03_Advanced/CoordinateTransformer.h
@@ -2,6 +2,7 @@
 #include <vector>
 #include "Vec2.h"
 #include "Graphics.h"
+#include "Drawalble.h"
 
 class CoordinateTransformer
 {
@@ -10,15 +11,12 @@ public:
 		:
 		gfx(gfx)
 	{}
-	void DrawClosedPolyline(std::vector<Vec2> poly, Color c)
+	void Draw(Drawable& drawable) const
 	{
-		Vec2 offset = { float(Graphics::ScreenHeight / 2), float(Graphics::ScreenHeight / 2) };
-		for (auto& v : poly)
-		{
-			v.y *= -1;	// FLIP the y axis direction
-			v += offset;
-		}
-		gfx.DrawClosedPolyline(poly, c);
+		const Vec2 offset = { float(Graphics::ScreenHeight / 2), float(Graphics::ScreenHeight / 2) };
+		drawable.ScaleIndipendent(1.0f, -1.0f);
+		drawable.Translate(offset);
+		drawable.Render(gfx);
 	}
 private:
 	Graphics& gfx;
